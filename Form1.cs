b96cef4ba6ee39cb82f21db9746e1b0c2249d0e7
Robert076 <robertberes06@gmail.com>
2025-05-09 13:27:30 +0300

    .

--- Form1.cs
+++ Form1.cs
@@ -2,22 +2,33 @@ using Microsoft.Data.SqlClient;
 using System;
 using System.Data;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace dbms
 {
     public partial class Form1 : Form
     {
-        string connectionString = @"Data Source=ASUS-PC\SQLEXPRESS;Initial Catalog=Supplements;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
-        SqlDataAdapter categoriesAdapter, supplementsAdapter;
-        DataSet categoriesDataSet = new DataSet();
-        DataSet supplementsDataSet = new DataSet();
-
-        int? selectedCategoryId = null;
-
+        private FormConfiguration _config;
+        private string _connectionString;
+        private SqlDataAdapter _categoriesAdapter, _supplementsAdapter;
+        private DataSet _categoriesDataSet = new DataSet();
+        private DataSet _supplementsDataSet = new DataSet();
+        private int? _selectedCategoryId = null;
 
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                _config = ConfigurationManager.GetConfiguration();
+                _connectionString = _config.ConnectionString;
+                this.Text = _config.FormCaption;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading configuration: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
         }
 
         public void TestConnectionButtonClick(object sender, EventArgs e)
@@ -29,7 +40,7 @@ namespace dbms
         {
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
                     connection.Open();
                     MessageBox.Show("Test connection succeeded.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -48,37 +59,37 @@ namespace dbms
 
         private void LoadAllCategories()
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
-                SqlCommand cmd = new SqlCommand("SELECT * FROM Categories", connection);
+                SqlCommand cmd = new SqlCommand(_config.MasterTable.SelectQuery, connection);
 
-                categoriesAdapter = new SqlDataAdapter(cmd);
-                categoriesDataSet.Clear();
+                _categoriesAdapter = new SqlDataAdapter(cmd);
+                _categoriesDataSet.Clear();
 
-                categoriesAdapter.Fill(categoriesDataSet, "Categories");
-                dataGridCategories.DataSource = categoriesDataSet.Tables["Categories"];
+                _categoriesAdapter.Fill(_categoriesDataSet, _config.MasterTable.TableName);
+                dataGridCategories.DataSource = _categoriesDataSet.Tables[_config.MasterTable.TableName];
                 connection.Close();
             }
         }
 
         private void LoadSupplementsBySelectedCategoryId()
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(_connectionString))
             {
-                if (this.selectedCategoryId == null || this.selectedCategoryId == -1)
+                if (this._selectedCategoryId == null || this._selectedCategoryId == -1)
                 {
                     MessageBox.Show("Please select a category first.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
 
-                SqlCommand cmd = new SqlCommand("SELECT s.SupplementID, SupplementName, SupplementDescription, SupplierID, SupplementStock, SupplementPrice FROM Supplements s WHERE s.CategoryID = @CategoryID", connection);
-                cmd.Parameters.AddWithValue("@CategoryID", this.selectedCategoryId);
+                SqlCommand cmd = new SqlCommand(_config.DetailTable.SelectQuery, connection);
+                cmd.Parameters.AddWithValue("@CategoryID", this._selectedCategoryId);
 
-                supplementsAdapter = new SqlDataAdapter(cmd);
-                supplementsDataSet.Clear();
-                supplementsAdapter.Fill(supplementsDataSet, "Supplements");
-                dataGridSupplements.DataSource = supplementsDataSet.Tables["Supplements"];
+                _supplementsAdapter = new SqlDataAdapter(cmd);
+                _supplementsDataSet.Clear();
+                _supplementsAdapter.Fill(_supplementsDataSet, _config.DetailTable.TableName);
+                dataGridSupplements.DataSource = _supplementsDataSet.Tables[_config.DetailTable.TableName];
                 connection.Close();
             }
         }
@@ -87,16 +98,16 @@ namespace dbms
         {
             if (dataGridCategories.CurrentRow != null)
             {
-                this.selectedCategoryId = Convert.ToInt32(dataGridCategories.CurrentRow.Cells["CategoryID"].Value);
+                this._selectedCategoryId = Convert.ToInt32(dataGridCategories.CurrentRow.Cells["CategoryID"].Value);
                 LoadSupplementsBySelectedCategoryId();
             }
         }
 
         private void AddRecordButtonClick(object sender, EventArgs e)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(_connectionString))
             {
-                if (this.selectedCategoryId == -1 || this.selectedCategoryId == null)
+                if (this._selectedCategoryId == -1 || this._selectedCategoryId == null)
                 {
                     MessageBox.Show("Please select a category in order to add a supplement first.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
@@ -105,19 +116,19 @@ namespace dbms
                 string supplementName = supplementNameTextBox.Text;
                 string supplementDescription = supplementDescriptionTextBox.Text;
 
-                SqlCommand command = new SqlCommand("INSERT INTO Supplements(SupplementName, SupplementDescription, SupplierId, SupplementStock, SupplementPrice, CategoryID) VALUES(@SupplementName, @SupplementDescription, @SupplierId, @SupplementStock, @SupplementPrice, @CategoryID )", conn);
                 if (supplementName == "" || supplementDescription == "")
                 {
                     MessageBox.Show("Please fill all the fields.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
 
+                SqlCommand command = new SqlCommand(_config.DetailTable.InsertQuery, conn);
                 command.Parameters.AddWithValue("@SupplementName", supplementName);
                 command.Parameters.AddWithValue("@SupplementDescription", supplementDescription);
                 command.Parameters.AddWithValue("@SupplierId", 2);
                 command.Parameters.AddWithValue("@SupplementStock", 100);
                 command.Parameters.AddWithValue("@SupplementPrice", 24.22);
-                command.Parameters.AddWithValue("@CategoryID", this.selectedCategoryId);
+                command.Parameters.AddWithValue("@CategoryID", this._selectedCategoryId);
 
                 conn.Open();
 
@@ -143,18 +154,17 @@ namespace dbms
 
         private void DeleteChildRowButtonClick(object sender, EventArgs e)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(_connectionString))
             {
                 if (dataGridSupplements.SelectedRows.Count > 0)
                 {
-                    // Ask for confirmation before deleting
                     DialogResult result = MessageBox.Show("Are you sure you want to delete this supplement?",
                         "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
                     if (result == DialogResult.Yes)
                     {
                         int supplementId = Convert.ToInt32(dataGridSupplements.SelectedRows[0].Cells["SupplementID"].Value);
-                        SqlCommand cmd = new SqlCommand("DELETE FROM Supplements WHERE SupplementID = @SupplementID", conn);
+                        SqlCommand cmd = new SqlCommand(_config.DetailTable.DeleteQuery, conn);
                         cmd.Parameters.AddWithValue("@SupplementID", supplementId);
                         try
                         {
@@ -172,10 +182,10 @@ namespace dbms
                         }
                         MessageBox.Show("Supplement deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
-                    else
-                    {
-                        MessageBox.Show("Please select a supplement to delete.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a supplement to delete.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
                 LoadSupplementsBySelectedCategoryId();
             }
@@ -183,7 +193,7 @@ namespace dbms
 
         private void UpdateButtonClick(object sender, EventArgs e)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(_connectionString))
             {
                 if (dataGridSupplements.SelectedRows.Count > 0)
                 {
@@ -194,7 +204,7 @@ namespace dbms
                     Int32 supplementStock = Convert.ToInt32(dataGridSupplements.SelectedRows[0].Cells["SupplementStock"].Value);
                     Int32 supplementPrice = Convert.ToInt32(dataGridSupplements.SelectedRows[0].Cells["SupplementPrice"].Value);
 
-                    SqlCommand command = new SqlCommand("UPDATE Supplements SET SupplementName = @SupplementName, SupplementDescription = @SupplementDescription, SupplementStock = @SupplementStock, SupplementPrice = @SupplementPrice WHERE SupplementID = @SupplementID", conn);
+                    SqlCommand command = new SqlCommand(_config.DetailTable.UpdateQuery, conn);
 
                     command.Parameters.AddWithValue("@SupplementName", supplementName);
                     command.Parameters.AddWithValue("@SupplementDescription", supplementDescription);
